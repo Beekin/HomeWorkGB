d4d25a62ab422f2c0b5b4268de22a3d894a95149
Anatoliy.Bykin <bykinlol@mail.ru>
2023-03-05 19:09:11 +0300

    Task004 (Max)

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое целое число: ");
+
+int userNumber_1 = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите второе целое число: ");
+
+int userNumber_2 = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите третье целое число: ");
+
+int userNumber_3 = int.Parse(Console.ReadLine() ?? "");
+
+int max = userNumber_1;
+
+int min = userNumber_1;
+
+
+
+if (userNumber_1 > userNumber_2)
+{
+    
+    max = userNumber_1;
+
+    min = userNumber_2;
+
+    if (userNumber_3 > max)
+
+    {
+
+        max = userNumber_3;
+
+        min = userNumber_2;
+
+    }
+
+}
+
+else 
+{
+    max = userNumber_2;
+
+    min = userNumber_1;
+
+    if (userNumber_3 > max)
+
+    {
+
+        max = userNumber_3;
+        
+        min = userNumber_1;
+
+    }
+
+    else
+
+    {
+
+        if (userNumber_3 < min)
+        {
+            min = userNumber_3;
+        }
+
+    }
+
+}
+
+Console.Write(" Max = " + max + " Min = " + min);
