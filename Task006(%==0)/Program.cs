841cfa21d9529b67a748cfa3859498d9577db031
Anatoliy.Bykin <bykinlol@mail.ru>
2023-03-05 19:15:53 +0300

    Task006(%==0)

---
+++ Task006(%==0)/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое целое число: ");
+
+int userInput = int.Parse(Console.ReadLine() ?? "");
+
+if (userInput % 2 == 0)
+
+{
+
+   Console.Write(userInput + " Четное число"); 
+
+}
+
+else
+
+{
+
+  Console.Write(userInput + " Нечетное число");   
+
+}
