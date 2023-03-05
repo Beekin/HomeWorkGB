602a500bd70d27a9559a1c823b66fa2d7ff330d1
Anatoliy.Bykin <bykinlol@mail.ru>
2023-03-05 19:18:33 +0300

    Task008(1-N)

---
+++ Task008(1-N)/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое целое число: ");
+
+int userInput = int.Parse(Console.ReadLine() ?? "");
+
+int i = 1;
+
+while (userInput >= i)
+
+{
+ if (i % 2 == 0)
+ {
+ Console.Write(i + ", ");
+ }
+
+
+    
+i++;
+
+}
