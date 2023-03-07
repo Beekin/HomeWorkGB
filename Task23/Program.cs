380b89ba2ec49ec3cef20e37e72b34fa8570f789
Anatoliy.Bykin <bykinlol@mail.ru>
2023-03-07 17:35:17 +0300

    Add Task23 to HomeWork2

---
+++ Task23/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите целое число: ");
+
+int userInput = int.Parse(Console.ReadLine() ?? "");
+
+
+
+while (userInput >= 1)
+{
+
+
+    int x = (int)Math.Pow(userInput, 3);
+
+    Console.WriteLine($"{userInput} -> {x} ");
+
+    userInput--;
+
+}
